a82fd838028a73727ecbb2c723caaa13961659bd
Cole Gawin <colegawin@gmail.com>
2021-09-16 17:36:35 -0500

    🔧 fix hover transition on SessionFormLogo

--- src/client/launch-session/components/SessionFormLogo.tsx
+++ src/client/launch-session/components/SessionFormLogo.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 
-import { Box, Heading } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 import { Logo } from "~/client/core/components/Logo";
 
 export const SessionFormLogo: React.FC = () => {
   return (
-    <Box>
-      <NextLink href={"/"} passHref>
-        <Heading as={"a"} size={"xl"} opacity={0.8} _hover={{ opacity: 1 }}>
-          <Logo />
-        </Heading>
-      </NextLink>
-    </Box>
+    <NextLink href={"/"} passHref>
+      <Heading as={"a"} size={"xl"} p={3} m={-3} opacity={0.8} _hover={{ opacity: 1 }}>
+        <Logo />
+      </Heading>
+    </NextLink>
   );
 };
