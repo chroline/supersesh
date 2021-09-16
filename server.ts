3b03dfbce7e8d2141b5c8eddecee0eb5d53ee041
Cole Gawin <colegawin@gmail.com>
2021-09-16 16:23:33 -0500

    🔧 better sol'n to #1 since server.js is already in "dist" dir, alias "~" to local "src" dir

--- server.ts
+++ server.ts
@@ -1,9 +1,6 @@
 import moduleAlias from "module-alias";
-
 import * as path from "path";
 
-console.log("~", path.join(__dirname, process.env.NODE_ENV === "production" ? "dist" : "src"));
-
-moduleAlias.addAlias("~", path.join(__dirname, process.env.NODE_ENV === "production" ? "dist" : "src"));
+moduleAlias.addAlias("~", path.join(__dirname, "src"));
 
 import("./src/server/main");
