dcbdefcaf724c975b1133181ae0723e4a304dc71
Cole Gawin <colegawin@gmail.com>
2021-09-16 23:33:23 -0500

    ⚡️ add send button to session page don't play sound on mobile

--- src/client/session-page/components/ChatForm.tsx
+++ src/client/session-page/components/ChatForm.tsx
@@ -1,11 +1,13 @@
 import React, { KeyboardEvent, useState } from "react";
 
-import { Box, Textarea, useToast } from "@chakra-ui/react";
+import { Flex, HStack, Icon, IconButton, Textarea, useToast } from "@chakra-ui/react";
+import { SendRounded } from "@material-ui/icons";
 import { useRouter } from "next/router";
 import TextareaAutosize from "react-autosize-textarea";
 
 import APIService from "~/client/core/services/api";
 import SessionDataService from "~/client/core/services/session-data";
+import isMobile from "~/client/session-page/util/isMobile";
 
 export const ChatForm: React.FC = () => {
   const sessionID = useRouter().query.sessionID as string;
@@ -14,51 +16,61 @@ export const ChatForm: React.FC = () => {
 
   const toast = useToast({ position: "bottom-right" });
 
-  async function editForm(e: KeyboardEvent<HTMLTextAreaElement>) {
+  async function submit() {
+    setValue("");
+
+    if (value == "" || !/\S/.test(value)) return;
+
+    !isMobile() && new Audio("/sfx/chat_send.m4a").play();
+    try {
+      const chat = {
+        date: new Date(),
+        userID: localStorage.getItem("name") as string,
+        msg: value,
+      };
+      await APIService.I.sendChat(sessionID, chat);
+      SessionDataService.I.addChat(chat);
+    } catch (e) {
+      toast({
+        title: "An error occurred",
+        description: (e as Error).message,
+        status: "error",
+        variant: "solid",
+        duration: 9000,
+        isClosable: true,
+      });
+    }
+  }
+
+  function editForm(e: KeyboardEvent<HTMLTextAreaElement>) {
     if (!e.shiftKey && e.key === "Enter") {
       e.preventDefault();
       e.stopPropagation();
-      setValue("");
-
-      if (value == "" || !/\S/.test(value)) return;
-
-      new Audio("/sfx/chat_send.m4a").play();
-      try {
-        const chat = {
-          date: new Date(),
-          userID: localStorage.getItem("name") as string,
-          msg: value,
-        };
-        await APIService.I.sendChat(sessionID, chat);
-        SessionDataService.I.addChat(chat);
-      } catch (e) {
-        toast({
-          title: "An error occurred",
-          description: (e as Error).message,
-          status: "error",
-          variant: "solid",
-          duration: 9000,
-          isClosable: true,
-        });
-      }
+      submit();
     }
   }
 
   return (
-    <Box w={"full"}>
-      <Textarea
-        value={value}
-        onChange={e => setValue(e.target.value)}
-        onKeyPressCapture={editForm}
-        placeholder={"send a chat"}
-        as={TextareaAutosize}
-        rows={1}
-        w={"full"}
-        variant={"unstyled"}
-        fontSize={"lg"}
-        py={6}
-        px={9}
-      />
-    </Box>
+    <HStack w={"full"} p={6} align={"flex-end"}>
+      <Flex minH={"full"} w={"full"} align={"center"}>
+        <Textarea
+          as={TextareaAutosize}
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          onKeyPressCapture={editForm}
+          placeholder={"send a chat"}
+          inputMode={"text"}
+          // @ts-ignore
+          enterKeyHint={"send"}
+          rows={1}
+          w={"full"}
+          variant={"unstyled"}
+          fontSize={"lg"}
+          py={0}
+          px={3}
+        />
+      </Flex>
+      <IconButton aria-label={"send"} icon={<Icon as={SendRounded} />} onClick={submit} colorScheme={"indigo"} />
+    </HStack>
   );
 };
